da1e4aad9514e8dc2b725369023878e34fd3f91a
mantu0tech <mantashaansari246@gmail.com>
2026-01-25 09:29:22 +0530

    adding the ui

--- src/main/java/com/codeboard/codeboardaws/controller/PersonController.java
+++ src/main/java/com/codeboard/codeboardaws/controller/PersonController.java
@@ -1,12 +1,19 @@
 package com.codeboard.codeboardaws.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class PersonController {
 
-	@RequestMapping("/getname")
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/getname")
+	@ResponseBody
 	public String getName() {
 		return "hello from the mantasha!!!";
 	}
